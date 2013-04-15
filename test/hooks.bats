cehoffman/rbenv

undo `assert_output_lines` in tests
It was a dumb idea and it wasn't even implemented perfectly.

test/hooks.bats
@@ -22,10 +22,12 @@ create_hook() {
   create_hook "$path2" exec "bueno.bash"
 
   RBENV_HOOK_PATH="$path1:$path2" run rbenv-hooks exec
-  assert_success "\
-    ${RBENV_TEST_DIR}/rbenv.d/exec/ahoy.bash
-    ${RBENV_TEST_DIR}/rbenv.d/exec/hello.bash
-    ${RBENV_TEST_DIR}/etc/rbenv_hooks/exec/bueno.bash"
+  assert_success
+  assert_output <<OUT
+${RBENV_TEST_DIR}/rbenv.d/exec/ahoy.bash
+${RBENV_TEST_DIR}/rbenv.d/exec/hello.bash
+${RBENV_TEST_DIR}/etc/rbenv_hooks/exec/bueno.bash
+OUT
 }
 
 @test "supports hook paths with spaces" {
@@ -35,9 +37,11 @@ create_hook() {
   create_hook "$path2" exec "ahoy.bash"
 
   RBENV_HOOK_PATH="$path1:$path2" run rbenv-hooks exec
-  assert_success "\
-    ${RBENV_TEST_DIR}/my hooks/rbenv.d/exec/hello.bash
-    ${RBENV_TEST_DIR}/etc/rbenv hooks/exec/ahoy.bash"
+  assert_success
+  assert_output <<OUT
+${RBENV_TEST_DIR}/my hooks/rbenv.d/exec/hello.bash
+${RBENV_TEST_DIR}/etc/rbenv hooks/exec/ahoy.bash
+OUT
 }
 
 @test "resolves relative paths" {

test/shell.bats
@@ -22,9 +22,11 @@ load test_helper
 
 @test "shell change invalid version" {
   run rbenv-sh-shell 1.2.3
-  assert_failure "\
-    rbenv: version \`1.2.3' not installed
-    return 1"
+  assert_failure
+  assert_output <<SH
+rbenv: version \`1.2.3' not installed
+return 1
+SH
 }
 
 @test "shell change version" {

test/which.bats
@@ -49,9 +49,12 @@ create_executable() {
   create_executable "2.0" "rspec"
 
   RBENV_VERSION=1.8 run rbenv-which rspec
-  assert_failure "\
-    rbenv: rspec: command not found
-    The \`rspec' command exists in these Ruby versions:
-      1.9
-      2.0"
+  assert_failure
+  assert_output <<OUT
+rbenv: rspec: command not found
+
+The \`rspec' command exists in these Ruby versions:
+  1.9
+  2.0
+OUT
 }

talentdeficit/erlenv

test/hooks.bats
@@ -22,10 +22,12 @@ create_hook() {
   create_hook "$path2" exec "bueno.bash"
 
   ERLENV_HOOK_PATH="$path1:$path2" run erlenv-hooks exec
-  assert_success "\
-    ${ERLENV_TEST_DIR}/erlenv.d/exec/ahoy.bash
-    ${ERLENV_TEST_DIR}/erlenv.d/exec/hello.bash
-    ${ERLENV_TEST_DIR}/etc/erlenv_hooks/exec/bueno.bash"
+  assert_success 
+	assert_output <<OUT
+${ERLENV_TEST_DIR}/erlenv.d/exec/ahoy.bash
+${ERLENV_TEST_DIR}/erlenv.d/exec/hello.bash
+${ERLENV_TEST_DIR}/etc/erlenv_hooks/exec/bueno.bash
+OUT
 }
 
 @test "supports hook paths with spaces" {
@@ -35,9 +37,11 @@ create_hook() {
   create_hook "$path2" exec "ahoy.bash"
 
   ERLENV_HOOK_PATH="$path1:$path2" run erlenv-hooks exec
-  assert_success "\
-    ${ERLENV_TEST_DIR}/my hooks/erlenv.d/exec/hello.bash
-    ${ERLENV_TEST_DIR}/etc/erlenv hooks/exec/ahoy.bash"
+  assert_success 
+	assert_output <<OUT
+${ERLENV_TEST_DIR}/my hooks/erlenv.d/exec/hello.bash
+${ERLENV_TEST_DIR}/etc/erlenv hooks/exec/ahoy.bash
+OUT
 }
 
 @test "resolves relative paths" {

test/shell.bats
@@ -22,9 +22,11 @@ load test_helper
 
 @test "shell change invalid release" {
   run erlenv-sh-shell 1.2.3
-  assert_failure "\
-  erlenv: release \`1.2.3' not installed
-  return 1"
+  assert_failure
+	assert_output <<SH
+erlenv: release \`1.2.3' not installed
+return 1
+SH
 }
 
 @test "shell change release" {

test/which.bats
@@ -49,9 +49,12 @@ create_executable() {
   create_executable "r17a" "rebar"
 
   ERLENV_RELEASE=r15b run erlenv-which rebar
-  assert_failure "\
-    erlenv: rebar: command not found
-    The \`rebar' command exists in these erlang releases:
-      r16b
-      r17a"
+  assert_failure
+	assert_output <<OUT
+erlenv: rebar: command not found
+
+The \`rebar' command exists in these erlang releases:
+  r16b
+  r17a
+OUT
 }