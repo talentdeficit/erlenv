cehoffman/rbenv

add tests for rehash, whence, which

test/rehash.bats
@@ -2,10 +2,17 @@
 
 load test_helper
 
+create_executable() {
+  local bin="${RBENV_ROOT}/versions/${1}/bin"
+  mkdir -p "$bin"
+  touch "${bin}/$2"
+  chmod +x "${bin}/$2"
+}
+
 @test "empty rehash" {
   assert [ ! -d "${RBENV_ROOT}/shims" ]
   run rbenv-rehash
-  assert_success
+  assert_success ""
   assert [ -d "${RBENV_ROOT}/shims" ]
   rmdir "${RBENV_ROOT}/shims"
 }
@@ -14,14 +21,32 @@ load test_helper
   mkdir -p "${RBENV_ROOT}/shims"
   chmod -w "${RBENV_ROOT}/shims"
   run rbenv-rehash
-  assert_failure
-  assert_output "rbenv: cannot rehash: ${RBENV_ROOT}/shims isn't writable"
+  assert_failure "rbenv: cannot rehash: ${RBENV_ROOT}/shims isn't writable"
 }
 
 @test "rehash in progress" {
   mkdir -p "${RBENV_ROOT}/shims"
   touch "${RBENV_ROOT}/shims/.rbenv-shim"
   run rbenv-rehash
-  assert_failure
-  assert_output "rbenv: cannot rehash: ${RBENV_ROOT}/shims/.rbenv-shim exists"
+  assert_failure "rbenv: cannot rehash: ${RBENV_ROOT}/shims/.rbenv-shim exists"
+}
+
+@test "creates shims" {
+  create_executable "1.8" "ruby"
+  create_executable "1.8" "rake"
+  create_executable "2.0" "ruby"
+  create_executable "2.0" "rspec"
+
+  assert [ ! -e "${RBENV_ROOT}/shims/ruby" ]
+  assert [ ! -e "${RBENV_ROOT}/shims/rake" ]
+  assert [ ! -e "${RBENV_ROOT}/shims/rspec" ]
+
+  run rbenv-rehash
+  assert_success ""
+
+  run ls "${RBENV_ROOT}/shims"
+  assert_success "\
+    rake
+    rspec
+    ruby"
 }

test/shell.bats
@@ -22,14 +22,13 @@ load test_helper
 
 @test "shell change invalid version" {
   run rbenv-sh-shell 1.2.3
-  assert_failure
-  assert_line "rbenv: version \`1.2.3' not installed"
-  assert_line "return 1"
+  assert_failure "\
+    rbenv: version \`1.2.3' not installed
+    return 1"
 }
 
 @test "shell change version" {
   mkdir -p "${RBENV_ROOT}/versions/1.2.3"
   run rbenv-sh-shell 1.2.3
   assert_success 'export RBENV_VERSION="1.2.3"'
-  refute_line "return 1"
 }

talentdeficit/erlenv

test/rehash.bats
@@ -2,10 +2,17 @@
 
 load test_helper
 
+create_executable() {
+  local bin="${ERLENV_ROOT}/releases/${1}/bin"
+  mkdir -p "$bin"
+  touch "${bin}/$2"
+  chmod +x "${bin}/$2"
+}
+
 @test "empty rehash" {
   assert [ ! -d "${ERLENV_ROOT}/shims" ]
   run erlenv-rehash
-  assert_success
+  assert_success ""
   assert [ -d "${ERLENV_ROOT}/shims" ]
   rmdir "${ERLENV_ROOT}/shims"
 }
@@ -14,14 +21,33 @@ load test_helper
   mkdir -p "${ERLENV_ROOT}/shims"
   chmod -w "${ERLENV_ROOT}/shims"
   run erlenv-rehash
-  assert_failure
-  assert_output "erlenv: cannot rehash: ${ERLENV_ROOT}/shims isn't writable"
+  assert_failure "erlenv: cannot rehash: ${ERLENV_ROOT}/shims isn't writable"
 }
 
 @test "rehash in progress" {
   mkdir -p "${ERLENV_ROOT}/shims"
   touch "${ERLENV_ROOT}/shims/.erlenv-shim"
   run erlenv-rehash
-  assert_failure
-  assert_output "erlenv: cannot rehash: ${ERLENV_ROOT}/shims/.erlenv-shim exists"
+  assert_failure "erlenv: cannot rehash: ${ERLENV_ROOT}/shims/.erlenv-shim exists"
 }
+
+@test "creates shims" {
+  create_executable "r15b" "erl"
+  create_executable "r15b" "erlc"
+  create_executable "r16b" "erl"
+  create_executable "r16b" "typer"
+
+  assert [ ! -e "${ERLENV_ROOT}/shims/erl" ]
+  assert [ ! -e "${ERLENV_ROOT}/shims/erlc" ]
+  assert [ ! -e "${ERLENV_ROOT}/shims/typer" ]
+
+  run erlenv-rehash
+  assert_success ""
+
+  run ls "${ERLENV_ROOT}/shims"
+  assert_success "\
+    erl
+		erlc
+		typer"
+}
+

test/shell.bats
@@ -22,14 +22,13 @@ load test_helper
 
 @test "shell change invalid release" {
   run erlenv-sh-shell 1.2.3
-  assert_failure
-  assert_line "erlenv: release \`1.2.3' not installed"
-  assert_line "return 1"
+  assert_failure "\
+  erlenv: release \`1.2.3' not installed
+  return 1"
 }
 
 @test "shell change release" {
   mkdir -p "${ERLENV_ROOT}/releases/1.2.3"
   run erlenv-sh-shell 1.2.3
   assert_success 'export ERLENV_RELEASE="1.2.3"'
-  refute_line "return 1"
 }